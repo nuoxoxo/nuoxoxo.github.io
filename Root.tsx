4c94ed43ddf21e826dddcc40c713a6d8da821e60
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-27 00:04:42 +0200

    io : init

---
+++ Root.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
+import App from './App';
+
+const Root = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+};
+
+export default Root
